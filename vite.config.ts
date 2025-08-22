b36a101d14454644d7968dc6b9823090321af557
Szymonexis <szymonkaszuba3@wp.pl>
2025-08-22 23:59:47 +0200

    remove aggressive cache stup from vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,14 +3,5 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [tailwindcss(), sveltekit()],
-	build: {
-		rollupOptions: {
-			output: {
-				entryFileNames: `assets/[name].[hash].${Date.now()}.js`,
-				chunkFileNames: `assets/[name].[hash].${Date.now()}.js`,
-				assetFileNames: `assets/[name].[hash].${Date.now()}.[ext]`
-			}
-		}
-	}
+	plugins: [tailwindcss(), sveltekit()]
 });
