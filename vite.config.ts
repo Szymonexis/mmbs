5219231466e7a441803b825d43c76f3205747f01
Szymonexis <szymonkaszuba3@wp.pl>
2025-08-22 23:46:15 +0200

    update types for services page

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,14 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [tailwindcss(), sveltekit()]
+	plugins: [tailwindcss(), sveltekit()],
+	build: {
+		rollupOptions: {
+			output: {
+				entryFileNames: `assets/[name].[hash].${Date.now()}.js`,
+				chunkFileNames: `assets/[name].[hash].${Date.now()}.js`,
+				assetFileNames: `assets/[name].[hash].${Date.now()}.[ext]`
+			}
+		}
+	}
 });
